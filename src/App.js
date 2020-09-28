0c07a3e4c24644c2b4c4d9f02d0a91bf5db48fc1
yunseop-dev <c.o.d.e@kakao.com>
2020-09-28 22:29:38 +0900

    코드 수정

--- src/App.js
+++ src/App.js
@@ -1,92 +1,81 @@
 import React, { useEffect, useState, useCallback } from "react";
 
+const LIMIT = 15;
+
 function App() {
-  console.log("🐛 App > Start");
-  const LIMIT = 15;
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
 
   const fetchAPI = useCallback(async () => {
-    console.log("🐛 App > fetchAPI", page);
-    setIsLoading(true);
-    const bestStories = await fetch(
-      "https://hacker-news.firebaseio.com/v0/beststories.json"
-    ).then((res) => res.json());
+    try {
+      setIsLoading(true);
+      const bestStories = await fetch(
+        "https://hacker-news.firebaseio.com/v0/beststories.json"
+      ).then((res) => res.json());
 
-    const result = await Promise.all(
-      bestStories
-        .filter(
-          (_, index) => LIMIT * (page - 1) <= index && index < LIMIT * page
-        )
-        .map((story) =>
-          fetch(
-            `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
-          ).then((res) => res.json())
-        )
-    );
+      const result = await Promise.all(
+        bestStories
+          .filter(
+            (_, index) => LIMIT * (page - 1) <= index && index < LIMIT * page
+          )
+          .map((story) =>
+            fetch(
+              `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
+            ).then((res) => res.json())
+          )
+      );
 
-    setIsLoading(false);
-    setItems((prev) => [...prev, ...result]);
+      setItems((prev) => [...prev, ...result]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
   }, [page]);
 
-  const handleScroll = useCallback(() => {
-    if (
-      window.innerHeight + document.documentElement.scrollTop !==
-        document.documentElement.offsetHeight ||
-      isLoading
-    ) {
-      return;
-    }
-    console.log("🐛 handleScroll out of if");
-    setPage(page + 1);
-    // fetchAPI(); // 왜 이럴까?
-  }, [isLoading, page]);
-
   useEffect(() => {
-    console.log("🐛 App > useEffect handleScroll");
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      console.log("🐛 App > useEffect handleScroll return");
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [handleScroll]);
-
-  useEffect(() => {
-    console.log("🐛 App > useEffect fetchAPI");
-    try {
-      fetchAPI();
-    } catch (error) {
-      setIsLoading(false);
-      setError(error);
-    }
+    fetchAPI();
   }, [fetchAPI]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const isReachedToBottom =
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight;
+      if (!isReachedToBottom || isLoading) {
+        return;
+      }
+      setPage((page) => page + 1);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (isLoading) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <main>
-        <h1>Hacker News</h1>
-        <ol>
-          {items.map((item, index) => (
-            <li key={index}>
-              <h4>
-                <a href={item.url}>{item.title}</a>
-              </h4>
-              <p>
-                {item.score} points by {item.by} {item.time} |{" "}
-                {item.kids.length} comments
-              </p>
-            </li>
-          ))}
-        </ol>
-      </main>
-    );
+    return <div>{error}</div>;
   }
+  return (
+    <main>
+      <h1>Hacker News</h1>
+      <ol>
+        {items.map((item, index) => (
+          <li key={index}>
+            <h4>
+              <a href={item.url}>{item.title}</a>
+            </h4>
+            <p>
+              {item.score} points by {item.by} {item.time} | {item.kids.length}{" "}
+              comments
+            </p>
+          </li>
+        ))}
+      </ol>
+    </main>
+  );
 }
 
 export default App;
