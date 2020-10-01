c19dac7615569e5600048b032156b8e590eee451
yunseop-dev <c.o.d.e@kakao.com>
2020-10-01 15:32:33 +0900

    hook을 나눠봄

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
-import React, { useEffect, useState, useCallback } from "react";
-
-const LIMIT = 15;
+import React, { useState } from "react";
+import useHackerNewsStoires from "./hooks/useHackerNewsStories";
+import useInfinityScroll from "./hooks/useInfinityScroll";
 
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
+  const { error, isLoading, items } = useHackerNewsStoires({ page });
+  function fetchMore() {
+    if (isLoading) return;
+    setPage((page) => page + 1);
+  }
 
-  const fetchAPI = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const bestStories = await fetch(
-        "https://hacker-news.firebaseio.com/v0/beststories.json"
-      ).then((res) => res.json());
-
-      const result = await Promise.all(
-        bestStories
-          .filter(
-            (_, index) => LIMIT * (page - 1) <= index && index < LIMIT * page
-          )
-          .map((story) =>
-            fetch(
-              `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
-            ).then((res) => res.json())
-          )
-      );
-
-      setItems((prev) => [...prev, ...result]);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [page]);
-
-  useEffect(() => {
-    fetchAPI();
-  }, [fetchAPI]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const isReachedToBottom =
-      window.innerHeight + document.documentElement.scrollTop ===
-      document.documentElement.offsetHeight;
-      if (!isReachedToBottom || isLoading) {
-        return;
-      }
-      setPage((page) => page + 1);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
+  useInfinityScroll({
+    action: fetchMore,
   });
 
   if (error) {
@@ -61,6 +20,7 @@ function App() {
   return (
     <main>
       <h1>Hacker News</h1>
+      {isLoading && <div>Loading...</div>}
       <ol>
         {items.map((item, index) => (
           <li key={index}>
---
+++ src/hooks/useHackerNewsStories.js
@@ -0,0 +1,46 @@
+import { useState, useEffect, useCallback } from "react";
+
+const LIMIT = 15;
+
+export default function useHackerNewsStoires({ page }) {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+  const fetchAPI = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const bestStories = await fetch(
+        "https://hacker-news.firebaseio.com/v0/beststories.json"
+      ).then((res) => res.json());
+
+      const result = await Promise.all(
+        bestStories
+          .filter(
+            (_, index) => LIMIT * (page - 1) <= index && index < LIMIT * page
+          )
+          .map((story) =>
+            fetch(
+              `https://hacker-news.firebaseio.com/v0/item/${story}.json?print=pretty`
+            ).then((res) => res.json())
+          )
+      );
+
+      setItems((prev) => [...prev, ...result]);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchAPI();
+  }, [fetchAPI]);
+
+  return {
+    error,
+    isLoading,
+    items,
+  };
+}
